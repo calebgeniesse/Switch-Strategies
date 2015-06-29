dda1f6801aefed5dc8b2c6d4c52d2267719865b7
Vineet Kosaraju <vineetsk1@gmail.com>
2015-06-29 14:54:01 -0700

    Adding more optimization variables for the amount to penalize by

--- vineetkosaraju_penalize_large_amounts_of_gc_pairs.py
+++ vineetkosaraju_penalize_large_amounts_of_gc_pairs.py
@@ -12,7 +12,7 @@ class Strategy(strategy_template.Strategy):
         self.url_ = "https://getsatisfaction.com/eternagame/topics/-strategy-market-switch-penalize-large-amounts-of-gc-pairs"
 
         # Default strategy parameters
-        self.default_params_ = [0.4, 0.2]
+        self.default_params_ = [0.4, 0.2, 1.0, 1.0]
 
         # Number of lines of code used to implement the strategy
         self.code_length_ = 4
@@ -25,6 +25,6 @@ class Strategy(strategy_template.Strategy):
     def score(self, design, params):
         perGC = float(design['gc'] / (design['gu'] + design['gc'] + design['ua']))
         
-        if perGC > params[0]:   return 100 - ((perGC - params[0])*100)
-        elif perGC < params[1]: return 100 - ((params[1] - perGC)*100)
+        if perGC > params[0]:   return 100 - ((perGC - params[0])*100*params[2])
+        elif perGC < params[1]: return 100 - ((params[1] - perGC)*100*params[3])
         else:                   return 100
