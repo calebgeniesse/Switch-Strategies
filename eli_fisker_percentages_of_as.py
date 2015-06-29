735814806a387b7e65b7edc5ee81537bcef5e9c3
Vineet Kosaraju <VineetK@DN52em1k.SUNet>
2015-06-29 14:54:01 -0700

    Adding more optimization variables for the amount to penalize by

--- eli_fisker_percentages_of_as.py
+++ eli_fisker_percentages_of_as.py
@@ -12,7 +12,7 @@ class Strategy(strategy_template.Strategy):
         self.url_ = "https://getsatisfaction.com/eternagame/topics/-strategy-market-switch-percentages-of-a-s"
 
         # Default strategy parameters
-        self.default_params_ = [0.29, 0.35, 0.31, 0.33, 1, 2]
+        self.default_params_ = [0.29, 0.35, 0.31, 0.33, 1, 2, 1.0, 1.0]
 
         # Number of lines of code used to implement the strategy
         self.code_length_ = 7
@@ -29,7 +29,7 @@ class Strategy(strategy_template.Strategy):
         score = 100
         if params[0] < perA < params[1]: score += params[4]
         if params[2] < perA < params[3]: score += params[5]
-        if perA > params[1]: score -= ((perA - params[1])*100)
-        if perA < params[0]: score -= ((params[0] - perA)*100)
+        if perA > params[1]: score -= ((perA - params[1])*100*params[6])
+        if perA < params[0]: score -= ((params[0] - perA)*100*params[7])
 
         return score
