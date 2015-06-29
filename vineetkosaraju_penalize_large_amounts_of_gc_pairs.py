0a1e63d45edd9e5d87b0191a83a3aed79dda6910
Vineet Kosaraju <VineetK@DN52em1k.SUNet>
2015-06-29 11:44:45 -0700

    fixing formatting

--- vineetkosaraju_penalize_large_amounts_of_gc_pairs.py
+++ vineetkosaraju_penalize_large_amounts_of_gc_pairs.py
@@ -1,26 +1,26 @@
 import strategy_template
 
 class Strategy(strategy_template.Strategy):
-	def __init__(self):
-		
-		strategy_template.Strategy.__init__(self)
-		
-		self.title_ = "[Strategy Market][Switch] Penalize large amounts of GC pairs" # Title of the strategy submission
-		self.author_ = "vineetkosaraju" # Author of the strategy submisison
-		
+    def __init__(self):
+
+        strategy_template.Strategy.__init__(self)
+
+        self.title_ = "[Strategy Market][Switch] Penalize large amounts of GC pairs" # Title of the strategy submission
+        self.author_ = "vineetkosaraju" # Author of the strategy submisison
+
         # URL where the strategy was initially submitted
         self.url_ = "https://getsatisfaction.com/eternagame/topics/-strategy-market-switch-penalize-large-amounts-of-gc-pairs"
-		
+
         # Default strategy parameters
         self.default_params_ = [0.4, 0.2]
-		
+
         # Number of lines of code used to implement the strategy
         self.code_length_ = 4
 
-		self.publishable_ = True
-		self.denormalized_ = True
-		self.comprehensive = False
-		
+        self.publishable_ = True
+        self.denormalized_ = True
+        self.comprehensive = False
+        
 
 	def score(self, design, params):
         perGC = float(design['gc'] / (design['gu'] + design['gc'] + design['ua']))
