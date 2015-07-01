61be05a65db075a4301570be1ee71eb3a7ef9742
Vineet Kosaraju <vineetsk1@gmail.com>
2015-07-01 12:44:12 -0700

    Updating with preliminary version of eli_fisker_obligatory_switching_base_pairs_in_MS2_FMN_switches

---
+++ eli_fisker_obligatory_switching_base_pairs_in_MS2_FMN_switches.py
@@ -0,0 +1,46 @@
+import eterna_utils
+import strategy_template
+
+class Strategy(strategy_template.Strategy):
+    def __init__(self):
+
+        strategy_template.Strategy.__init__(self)
+
+        self.title_ = "[Strategy Market] [Switch] Elements: Obligatory switching base pairs in MS2/FMN switches" # Title of the strategy submission
+        self.author_ = "Eli Fisker" # Author of the strategy submisison
+
+        # URL where the strategy was initially submitted
+        self.url_ = "https://getsatisfaction.com/eternagame/topics/-strategy-market-switch-obligatory-switching-base-pairs-in-ms2-fmn-switches"
+
+        # Default strategy parameters
+        self.default_params_ = [10, 12, 2.0, 1.0, -1.0]
+
+        # Number of lines of code used to implement the strategy
+        self.code_length_ = 14
+
+        self.publishable_ = True
+        self.denormalized_ = True
+        self.comprehensive = False
+        
+
+    def score(self, design, params):
+
+        score = 100
+        seqlength = len(design['sequence'])
+        seq_range = range(0, seqlength)
+
+        switchbp = 0
+
+        for i in seq_range:
+            if design['secstruct1'][i] == '(': # uncomment this line to include double counting: or design['secstruct1'][i] == ')':
+                # base paired
+                pair1 = design['sequence'][design['pairmap1'][i]]
+                pair2 = design['sequence'][design['pairmap2'][i]]
+                if pair1 != pair2: switchbp += 1
+
+        if ("miRNA" in design['labtitle']): score = eterna_utils.UNSCORABLE
+        elif switchbp <= params[0]: score += params[2]
+        elif switchbp <= params[1]: score += params[3]
+        elif switchbp > params[1]: score += ((switchbp - params[1])*params[4])
+
+        return score
