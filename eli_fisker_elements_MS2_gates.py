a23747442358c835e1c038d7a0c4243ed42cee06
Vineet Kosaraju <vineetsk1@gmail.com>
2015-07-10 09:42:07 -0700

    last PEP8 changes

--- eli_fisker_elements_MS2_gates.py
+++ eli_fisker_elements_MS2_gates.py
@@ -2,6 +2,7 @@ import eterna_utils
 import strategy_template
 from math import pow
 
+
 class Strategy(strategy_template.Strategy):
     def __init__(self):
 
@@ -12,13 +13,15 @@ class Strategy(strategy_template.Strategy):
         self.author_ = "Eli Fisker"
 
         # URL where the strategy was initially submitted
-        self.url_ = "https://getsatisfaction.com/eternagame/topics/-strategy-market-switch-elements-ms2-gates"
+        self.url_ = ("https://getsatisfaction.com/eternagame/topics/"
+                     "-strategy-market-switch-elements-ms2-gates")
 
         # Default strategy parameters
-        self.default_params_ = [2, 3.0, -1.0, 1.0, 4, -1.0, 2, 3.0, 5, -1.0, 6, 9, 3.0, -1.0]
+        self.default_params_ = [2, 3.0, -1.0, 1.0, 4, -1.0, 2,
+                                3.0, 5, -1.0, 6, 9, 3.0, -1.0]
 
         # Number of lines of code used to implement the strategy
-        self.code_length_ = 29
+        self.code_length_ = LOLTODO
 
         self.publishable_ = True
         self.denormalized_ = True
@@ -40,28 +43,36 @@ class Strategy(strategy_template.Strategy):
 
                     # Turn off labs
                     if("Same State" not in design['puztitle']):
-                        if elem.type_ == eterna_utils.RNAELEMENT_STACK and length >= params[0]:
-                            score += params[1] + (params[2] * (length - params[0]))
+                        if (elem.type_ == eterna_utils.RNAELEMENT_STACK and
+                                length >= params[0]):
+                            score += ((params[2] * (length - params[0])) +
+                                      params[1])
                         elif elem.type_ == eterna_utils.RNAELEMENT_LOOP:
                             score += params[3]
 
-                    # Turn on labs 
-                    elif("Exclusion" in design['puztitle'] and elem.type_ == eterna_utils.RNAELEMENT_STACK):
-                        if abs(minpos - minind) <= params[4] and abs(maxpos - maxind) <= params[4]:
+                    # Turn on labs
+                    elif("Exclusion" in design['puztitle'] and
+                            elem.type_ == eterna_utils.RNAELEMENT_STACK):
+                        if (abs(minpos - minind) <= params[4] and
+                                abs(maxpos - maxind) <= params[4]):
                             score += params[5] * pow(params[6], length)
 
                     # miRNA lab
-                    elif "miRNA" in design['labtitle'] and elem.type_ == eterna_utils.RNAELEMENT_STACK:
+                    elif ("miRNA" in design['labtitle'] and
+                            elem.type_ == eterna_utils.RNAELEMENT_STACK):
                         # miRNA - turn on
                         if "turn-off" not in design['puztitle']:
-                            score += params[7]+ (abs(length - params[8]) * params[9])
+                            score += ((abs(length - params[8]) * params[9]) +
+                                      params[7])
 
                         # miRNA - turn off
                         else:
                             if length > params[11]:
-                                score += params[12] + ((length - params[11]) * params[13])
+                                score += ((length - params[11]) * params[13] +
+                                          params[12])
                             elif length < params[10]:
-                                score += params[12] + ((length - params[10]) * params[13])
+                                score += ((length - params[10]) * params[13] +
+                                          params[12])
                             else:
                                 score += params[12]
 
